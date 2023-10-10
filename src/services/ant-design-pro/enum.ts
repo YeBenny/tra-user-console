da9278e64850d855ec6ab988f3d95e3d9a6c3031
YE WEIBIN <yeshinebenny@gmail.com>
2023-10-10 17:12:24 +0800

    feat: add enum and update typings

---
+++ src/services/ant-design-pro/enum.ts
@@ -0,0 +1,16 @@
+export enum LoginType {
+  Unknown = 0,
+  Password = 1,
+  SsoLogin = 2,
+}
+
+export enum LoginIdType {
+  Unknown = 0,
+  Email = 1,
+  QYWXUserId = 2,
+}
+
+export enum UserStatus {
+  Active = 1,
+  Inactive = 2,
+}
