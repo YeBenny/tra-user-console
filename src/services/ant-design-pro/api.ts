78cdedf0e997bdf8e60d57e3ba96c495ba92e8b6
YE WEIBIN <yeshinebenny@gmail.com>
2023-10-31 21:50:39 +0800

    chore: add series api and typings

--- src/services/ant-design-pro/api.ts
+++ src/services/ant-design-pro/api.ts
@@ -24,7 +24,18 @@ export async function login(body: API.LoginParams, options?: { [key: string]: an
   });
 }
 
-/** 获取应用程序列表 POST /app/list */
+/** 获取用户组织 GET / */
+export async function getOrgnization(options?: { [key: string]: any }) {
+  return request<API.ResponseResult<API.Organization>>('/organizations', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    ...(options || {}),
+  });
+}
+
+/** 获取应用程序列表 POST /apps/list */
 export async function getAppList(params: API.PageParams, options?: { [key: string]: any }) {
   const current = params.current ?? 1;
   const pageSize = params.pageSize ?? 10;
@@ -102,17 +113,283 @@ export async function updateApp(
   });
 }
 
-/** 获取Secret GET /app/secret */
+/** 获取Secret GET /apps/get-secret */
 export async function getSecret(
   body: API.GetApplicationSecretParams,
   options?: { [key: string]: any },
 ) {
-  return request<API.ResponseResult<string>>('/apps/secret', {
+  return request<API.ResponseResult<string>>('/apps/get-secret', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     data: body,
+    ...(options || {}),
+  });
+}
+
+/** 获取系列列表 POST /apps/series/list */
+export async function getSeriesList(params: API.PageParams, options?: { [key: string]: any }) {
+  const current = params.current ?? 1;
+  const pageSize = params.pageSize ?? 10;
+  const body = {
+    pageInfo: {
+      startIndex: (current - 1) * pageSize + 1,
+      pageSize: pageSize,
+    },
+  };
+  const msg = await request<API.ResponseResult<API.SeriesList>>('/apps/series/list', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  });
+  const data = msg.data;
+  const result = data.result;
+  result?.forEach((item) => {
+    item.startUnixTime = Math.max(0, item.startUnixTime ?? 0) * 1000;
+    item.endUnixTime = Math.max(0, item.endUnixTime ?? 0) * 1000;
+  });
+  return {
+    data: result,
+    total: data.pageInfo?.totalRows,
+    success: true,
+    pageSize: pageSize,
+    current: current,
+  };
+}
+
+/** 获取系列 POST /apps/series/get */
+export async function getSeries(body: API.GetSeriesParams, options?: { [key: string]: any }) {
+  const msg = await request<API.ResponseResult<API.SeriesListItem>>('/apps/series/get', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  });
+  msg.data.startUnixTime = Math.max(0, msg.data.startUnixTime ?? 0) * 1000;
+  msg.data.endUnixTime = Math.max(0, msg.data.endUnixTime ?? 0) * 1000;
+  return msg;
+}
+
+/** 新建系列 POST /apps/series */
+export async function createSeries(body: API.CreateSeriesParams, options?: { [key: string]: any }) {
+  return request<API.ResponseResult<API.SeriesListItem>>('/apps/series', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  });
+}
+
+/** 更新系列 PUT /apps/series */
+export async function updateSeries(body: API.UpdateSeriesParams, options?: { [key: string]: any }) {
+  return request<API.ResponseResult<API.SeriesListItem>>('/apps/series', {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  });
+}
+
+/** 获取系列模版列表 POST /apps/series/list-activity-candidats */
+export async function getSeriesActivityCandidatesList(options?: { [key: string]: any }) {
+  return request<API.ResponseResult<API.ActivityCandidateListItem[]>>(
+    '/apps/series/list-activity-candidates',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      ...(options || {}),
+    },
+  );
+}
+
+/** 获取系列模版列表 POST /apps/series/list-contract-candidats */
+export async function getSeriesContractCandidatesList(
+  body: API.GetContractCandidateListParams,
+  options?: { [key: string]: any },
+) {
+  return request<API.ResponseResult<API.ContractCandidateListItem[]>>(
+    '/apps/series/list-contract-candidates',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: body,
+      ...(options || {}),
+    },
+  );
+}
+
+/** 新建TRA POST /apps/tras */
+export async function createTras(body: API.CreateTrasParams, options?: { [key: string]: any }) {
+  return request<API.ResponseResult<API.TraListItem[]>>('/tras', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  });
+}
+
+/** 更新TRA POST /apps/tras */
+export async function updateTras(body: API.UpdateTrasParams, options?: { [key: string]: any }) {
+  return request<API.ResponseResult<API.TraListItem[]>>('/tras', {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  });
+}
+
+/** 获取TRA POST /tras/list */
+export async function getTra(params: API.GetTraParams, options?: { [key: string]: any }) {
+  const current = 1;
+  const pageSize = 1;
+  const body = {
+    pageInfo: {
+      startIndex: (current - 1) * pageSize + 1,
+      pageSize: pageSize,
+    },
+    filters: [
+      {
+        name: 'id',
+        operator: 'equal',
+        value: params.id,
+        valueType: 'string',
+      },
+    ],
+  };
+  const msg = await request<API.ResponseResult<API.TraList>>('/tras/list', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  });
+  const data = msg.data;
+  const result = data.result;
+  if (result.length > 0) {
+    return {
+      data: result[0],
+      success: true,
+    };
+  } else {
+    return {
+      data: null,
+      success: false,
+    };
+  }
+}
+
+/** 获取TRA列表 POST /tras/list */
+export async function getTraList(params: API.TraPageParams, options?: { [key: string]: any }) {
+  const current = params.current ?? 1;
+  const pageSize = params.pageSize ?? 10;
+  const body = {
+    pageInfo: {
+      startIndex: (current - 1) * pageSize + 1,
+      pageSize: pageSize,
+    },
+    filters: [
+      {
+        name: 'seriesId',
+        operator: 'equal',
+        value: params.seriesId,
+        valueType: 'string',
+      },
+    ],
+  };
+  const msg = await request<API.ResponseResult<API.TraList>>('/tras/list', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  });
+  const data = msg.data;
+  const result = data.result;
+  return {
+    data: result,
+    total: data.pageInfo?.totalRows,
+    success: true,
+    pageSize: pageSize,
+    current: current,
+  };
+}
+
+/** 新建兑换规则 POST /apps/tras */
+export async function createRedemptionRule(
+  body: API.CreateRedemptionRuleParams,
+  options?: { [key: string]: any },
+) {
+  return request<API.ResponseResult<API.RedemptionListItem[]>>('/apps/series/redemptions', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  });
+}
+
+/** 更新兑换规则 POST /apps/tras */
+export async function updateRedemptionRule(
+  body: API.UpdateRedemptionRuleParams,
+  options?: { [key: string]: any },
+) {
+  return request<API.ResponseResult<API.RedemptionListItem[]>>('/apps/series/redemptions', {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  });
+}
+
+/** 获取兑换规则列表 POST /tras/list */
+export async function getRedemptionList(
+  params: API.GetRedemptionListParams,
+  options?: { [key: string]: any },
+) {
+  return await request<API.ResponseResult<API.RedemptionListItem[]>>(
+    '/apps/series/redemptions/list',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: params,
+      ...(options || {}),
+    },
+  );
+}
+
+/** 获取文件上传URL POST /files/upload */
+export async function uploadFile(formData: FormData, options?: { [key: string]: any }) {
+  return request<API.ResponseResult<{ [key: string]: any }>>('/files/upload', {
+    method: 'POST',
+    headers: {
+      'Content-type': 'multipart/form-data',
+    },
+    data: formData,
     ...(options || {}),
   });
 }
--- src/services/ant-design-pro/typings.d.ts
+++ src/services/ant-design-pro/typings.d.ts
@@ -2,16 +2,50 @@
 /* eslint-disable */
 
 declare namespace API {
+  type UserStatusUnknown = 0;
+  type UserStatusActive = 1;
+  type UserStatusInActive = 2;
+  type UserStatus = UserStatusUnknown | UserStatusActive | UserStatusInActive;
+
+  type LoginTypeUnknown = 0;
+  type LoginTypePassword = 1;
+  type LoginTypeSSO = 2;
+  type LoginType = LoginTypeUnknown | LoginTypePassword | LoginTypeSSO;
+
+  type LoginIdTypeUnknown = 0;
+  type LoginIdTypeEmail = 1;
+  type LoginIdTypeWecomId = 2;
+  type LoginIdType = LoginIdTypeUnknown | LoginIdTypeEmail | LoginIdTypeWecomId;
+
+  type TemplateTypeUnknown = 0;
+  type TemplateTypeEvent = 1;
+  type TemplateTypeSignMethod = 2;
+  type TemplateType = TemplateTypeUnknown | TemplateTypeEvent | TemplateTypeSignMethod;
+
+  type TemplateSubTypeUnknown = 0;
+  type TemplateSubType = TemplateSubTypeUnknown;
+
+  type TemplateStatusUnknown = 0;
+  type TemplateStatusActive = 1;
+  type TemplateStatusType = TemplateStatusUnknown | TemplateStatusActive;
+
   type CurrentUser = {
     email: string;
     id: string;
     nickname: string;
-    userStatus: number;
+    userStatus: UserStatus;
+    permissionList?: string[];
   };
 
-  type LoginType = 0 | 1 | 2;
-
-  type LoginIdType = 0 | 1 | 2;
+  type Organization = {
+    createdAt: string;
+    description: string;
+    id: string;
+    inmeritsSceneId: string;
+    name: string;
+    updatedAt: number;
+    version: number;
+  };
 
   type ResponseResult<T> = {
     errorCode: number;
@@ -59,6 +93,18 @@ declare namespace API {
     pageSize?: number;
   };
 
+  type TemplatePageParams = {
+    current?: number;
+    pageSize?: number;
+    type?: number;
+  };
+
+  type TraPageParams = {
+    current?: number;
+    pageSize?: number;
+    seriesId?: string;
+  };
+
   type ApplicationListItem = {
     id: string;
     name: string;
@@ -72,5 +118,293 @@ declare namespace API {
   type ApplicationList = {
     result: ApplicationListItem[];
     pageInfo: PageInfoParams;
+  };
+
+  type GetSeriesParams = {
+    id: string;
+  };
+
+  type GetTraParams = {
+    id: string;
+  };
+
+  type GetRedemptionListParams = {
+    seriesId: string;
+  };
+
+  type CreateSeriesParams = {
+    appId: string;
+    title: string;
+    description: string;
+    image: string;
+    startUnixTime: number;
+    endUnixTime: number;
+    status: number;
+  };
+
+  type UpdateSeriesParams = {
+    id: string;
+    appId: string;
+    title: string;
+    description: string;
+    image: string;
+    startUnixTime: number;
+    endUnixTime: number;
+    status: number;
+    version: number;
+    updateFields: string[];
+  };
+
+  type SeriesListItem = {
+    id: string;
+    appId: string;
+    createdBy: string;
+    title: string;
+    description: string;
+    image: string;
+    startUnixTime: number;
+    endUnixTime: number;
+    status: number;
+    seriesState: number;
+    nodePath: string;
+    version: number;
+  };
+
+  type SeriesList = {
+    result: SeriesListItem[];
+    pageInfo: PageInfoParams;
+  };
+
+  type ActivityCandidateListItem = {
+    id: string;
+    name: string;
+    description: string;
+    logo: string;
+    sceneId: string;
+  };
+
+  type GetContractCandidateListParams = {
+    inmeritsNodePath: string;
+  };
+
+  type ContractCandidateListItem = {
+    id: string;
+    name: string;
+    address: string;
+    assetType: string;
+    contractType: string;
+    blockchainId: string;
+    blockchainType: string;
+    issueRules?: InssueRule[];
+  };
+
+  type InssueRule = {
+    assetAmount: number;
+    customMetadata: boolean;
+    dynamicAmount: boolean;
+    issueRuleEnable: boolean;
+    issueRuleIssueLimit: number;
+    issueRuleName: string;
+    materialList: Material[];
+    materialVisible: boolean;
+    amount: number;
+    issueFuncName: string;
+    isCombineIssue: boolean;
+    enableIssueNumber: number;
+    finalIssueNumber: number;
+    enableIssue: boolean;
+  };
+
+  type Material = {
+    name: string;
+    source: string;
+    fileType: string;
+    fileFormat: string;
+    category: string;
+    type: string;
+    amount: number;
+    writeOff: boolean;
+    destroy: boolean;
+    style: string;
+    factor: number;
+    mode: string;
+    value: number;
+    contentType: string;
+    content: string;
+    parentAssetId: string;
+    id: string;
+    combinationType: string;
+    writeOffValue: number;
+  };
+
+  type CreateTrasParams = {
+    seriesNodePath: string;
+    seriesVersion: number;
+    tras: CreateTrasListItem[];
+  };
+
+  type CreateTrasListItem = {
+    seriesId: string;
+    title: string;
+    description: string;
+    image: string;
+    contractAddress: string;
+    issueRuleId: string;
+  };
+
+  type UpdateTrasParams = {
+    items: UpdateTrasListItem[];
+  };
+
+  type UpdateTrasListItem = {
+    tra: {
+      id: string;
+      title: string;
+      description: string;
+      image: string;
+      version: number;
+    };
+    updateFields: string[];
+  };
+
+  type CreateRedemptionRuleParams = {
+    items: CreateRedemptionRuleItem[];
+  };
+
+  type CreateRedemptionRuleItem = {
+    redemptionRule: CreateRedemptionRule;
+    redemptionTraInfos: CreateRedemptionTraInfoItem[];
+  };
+
+  type CreateRedemptionRule = {
+    seriesId: string;
+    title: string;
+    description: string;
+    image: string;
+  };
+
+  type CreateRedemptionTraInfoItem = {
+    traId: string;
+    quantity: number;
+  };
+
+  type UpdateRedemptionRuleParams = {
+    items: UpdateRedemptionRuleItem[];
+  };
+
+  type UpdateRedemptionRuleItem = {
+    redemptionRule: UpdateRedemptionRule;
+    redemptionTraInfos: UpdateRedemptionTraInfoItem[];
+  };
+
+  type UpdateRedemptionRule = {
+    id: string;
+    seriesId: string;
+    title: string;
+    description: string;
+    image: string;
+    version: number;
+  };
+
+  type UpdateRedemptionTraInfoItem = {
+    id: string;
+    redemptionId: string;
+    traId: string;
+    quantity: number;
+    version: number;
+  };
+
+  type Filter = {
+    name: string;
+    operator: string;
+    value: string;
+    valueType: string;
+  };
+
+  type TraListItem = {
+    id: string;
+    seriesId: string;
+    title: string;
+    description: string;
+    image: string;
+    contractAddress: string;
+    issueRuleId: string;
+    combineRuleId: string;
+    version: number;
+    updatedAt: number;
+    createdAt: number;
+  };
+
+  type TraList = {
+    result: TraListItem[];
+    pageInfo: PageInfoParams;
+  };
+
+  type RedemptionInfoTraListItem = {
+    id: string;
+    title: string;
+    description: string;
+    image: string;
+    quantity: number;
+  };
+
+  type RedemptionListItem = {
+    redemptionRule: RedemptionRule;
+    redemptionTraInfos: RedemptionTraInfo[];
+  };
+
+  type RedemptionRule = {
+    id: string;
+    seriesId: string;
+    title: string;
+    description: string;
+    image: string;
+    version: number;
+    updatedAt: number;
+    createdAt: number;
+  };
+
+  type RedemptionTraInfo = {
+    id: string;
+    redemptionId: string;
+    traId: string;
+    quantity: number;
+    version: number;
+    createdAt: number;
+    updatedAt: number;
+  };
+
+  type TemplateList = {
+    result: TemplateListItem[];
+    pageInfo: PageInfoParams;
+  };
+
+  type TemplateListItem = {
+    id: string;
+    title: string;
+    description: string;
+    image: string;
+    content: string;
+    status: TemplateStatusType;
+    type: TemplateType;
+    subType: TemplateSubType;
+    version: number;
+    updatedAt: number;
+    createdAt: number;
+  };
+
+  type TemplateInstance = {
+    templateId: string;
+    templateContent: string;
+    templateType: TemplateType;
+    templateSubType: TemplateSubType;
+  };
+
+  type FieldListItem = {
+    title: string;
+    description: string;
+    type: string;
+    key: string;
+    children?: FieldListItem[];
   };
 }
